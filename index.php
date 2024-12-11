290a593b5b6c5f4cab8e175f19fb69228f84fa19
RAVINARAYAN <ravinarayan.b9510@gmail.com>
2024-12-11 15:08:29 +0530

    commit changes

--- index.php
+++ index.php
@@ -177,24 +177,5 @@
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
-<script>
-    (function() {
-        'use strict'
-        window.addEventListener('load', function() {
-            var forms = document.getElementsByClassName('needs-validation')
-            var validation = Array.prototype.filter.call(forms, function(form) {
-                form.addEventListener('submit', function(event) {
-                    if (form.checkValidity() === false) {
-                        event.preventDefault()
-                        event.stopPropagation()
-                    }
-                    form.classList.add('was-validated')
-                }, false)
-            })
-        }, false)
-    })();
-</script>
-
 </body>
 </html>
